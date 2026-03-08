df6ce4a4da4d474d5b2d36529b67d31ad30282fa
Vladyslav Sheludko <128414119+sheludkovladyslav@users.noreply.github.com>
2026-03-08 13:58:16 -0400

    refactored the code to redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
+  </Provider>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 import counterReducer from "../redux/counterSlice";
 import themeSwitchReducer from "../redux/themeSlice";
 import todoReducer from "../redux/TodoSlice";
@@ -10,17 +13,27 @@ import clickReducer from "./clickSlice";
 import usersReducer from "./usersSlice";
 import formReducer from "./formSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    theme: themeSwitchReducer,
-    todo: todoReducer,
-    modal: modalReducer,
-    cart: cartReducer,
-    language: languageReducer,
-    notifications: notificationsReducer,
-    click: clickReducer,
-    users: usersReducer,
-    form: formReducer,
-  },
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  theme: themeSwitchReducer,
+  todo: todoReducer,
+  modal: modalReducer,
+  cart: cartReducer,
+  language: languageReducer,
+  notifications: notificationsReducer,
+  click: clickReducer,
+  users: usersReducer,
+  form: formReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+export const persistor = persistStore(store);
